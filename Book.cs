bb50a8ef512be7b99a74c07d916ed63b5baa2d42
totomatix <theo.ollivier@gmail.com>
2025-10-10 10:18:50 +0200

    First Version

---
+++ Book.cs
@@ -0,0 +1,12 @@
+namespace SimpleAPI;
+
+public class Book
+{
+    public int id{ get; set; }
+
+    public string isbn { get; set; }
+
+    public string titre { get; set; }
+
+    public string description { get; set; }
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,35 @@
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace SimpleAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly string _connexionString;
+
+    public BooksController(IConfiguration configuration)
+    {
+        // récupération de la chaîne de connexion dans la configuration
+        _connexionString = configuration.GetConnectionString("DefaultConnection")!;
+        // si la chaîne de connexionn'a pas été trouvé => déclenche une exception => code http 500 retourné
+        if (string.IsNullOrEmpty(_connexionString))
+        {
+            throw new InvalidOperationException("Error : Connexion string not found ! ");
+        }
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Book>> Get()
+    {
+        string query = "SELECT id, isbn, titre, description FROM public.book;";
+        IEnumerable<Book> books;
+        using (var connexion = new NpgsqlConnection(_connexionString))
+        {
+            books = await connexion.QueryAsync<Book>(query);
+        }
+        return books;
+    }
+}
